e4325f9c4a628b7f2ae287ab20fc51ca337c3f98
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-01-28 12:04:24 -0500

    Inline MainView into MainController (#6)
    * Inline MainView into MainController
    
    * Remove MainView
    
    * Remove MainView.stories

--- ui/src/components/MainController.tsx
+++ ui/src/components/MainController.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import MainView from './MainView';
+import { Container, Grid, Header, Icon, Segment, Divider } from 'semantic-ui-react';
 import { Party } from '@digitalasset/daml-json-types';
 import { User } from '../daml/create-daml-app/User';
 import { useParty, useReload, usePseudoExerciseByKey, useFetchByKey, useQuery } from '../daml-react-hooks';
+import UserList from './UserList';
+import PartyListEdit from './PartyListEdit';
 
-/**
- * React component to control the `MainView`.
- */
 const MainController: React.FC = () => {
   const party = useParty();
-  const myUser = useFetchByKey(User, () => party, [party]);
-  const allUsers = useQuery(User, () => ({}), []);
+  const myUser = useFetchByKey(User, () => party, [party]).contract?.payload;
+  const allUsers = useQuery(User, () => ({}), []).contracts.map((user) => user.payload);
   const reload = useReload();
 
   const [exerciseAddFriend] = usePseudoExerciseByKey(User.AddFriend);
@@ -39,15 +38,56 @@ const MainController: React.FC = () => {
     return () => clearInterval(interval);
   }, [reload]);
 
-  const props = {
-    myUser: myUser.contract?.payload,
-    allUsers: allUsers.contracts.map((user) => user.payload),
-    onAddFriend: addFriend,
-    onRemoveFriend: removeFriend,
-    onReload: reload,
-  };
+  return (
+    <Container>
+      <Grid centered columns={2}>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Header as='h1' size='huge' color='blue' textAlign='center' style={{padding: '1ex 0em 0ex 0em'}}>
+                {myUser ? `Welcome, ${myUser.party}!` : 'Loading...'}
+            </Header>
 
-  return MainView(props);
+            <Segment>
+              <Header as='h2'>
+                <Icon name='user' />
+                <Header.Content>
+                  {myUser?.party ?? 'Loading...'}
+                  <Header.Subheader>Me and my friends</Header.Subheader>
+                </Header.Content>
+              </Header>
+              <Divider />
+              <PartyListEdit
+                parties={myUser?.friends ?? []}
+                onAddParty={addFriend}
+                onRemoveParty={removeFriend}
+              />
+            </Segment>
+            <Segment>
+              <Header as='h2'>
+                <Icon name='globe' />
+                <Header.Content>
+                  The Network
+                  <Icon
+                    link
+                    name='sync alternate'
+                    size='small'
+                    style={{marginLeft: '0.5em'}}
+                    onClick={reload}
+                  />
+                  <Header.Subheader>Others and their friends</Header.Subheader>
+                </Header.Content>
+              </Header>
+              <Divider />
+              <UserList
+                users={allUsers.sort((user1, user2) => user1.party.localeCompare(user2.party))}
+                onAddFriend={addFriend}
+              />
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
 }
 
 export default MainController;
