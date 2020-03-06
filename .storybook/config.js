29c41c2ec3862a30c4ef238a560869e9d984ba3a
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-03-06 14:40:18 +0100

    Remove more stuff related to docker and storybook (#55)
    Also remove some other useless files and clean up the package.json file
    while we're at removing the storybook stuff from it. And the
    `@types/...` depenedencies should definitely go into the
    `devDependencies`.
    
    This is part of https://github.com/digital-asset/daml/issues/4866.

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-import 'semantic-ui-css/semantic.min.css';
-import '../src/index.css';
-
-const req = require.context('../src/components', true, /.stories.tsx$/);
-function loadStories() {
-  req.keys().forEach(req);
-}
-configure(loadStories, module);
